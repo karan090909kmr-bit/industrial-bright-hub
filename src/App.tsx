58bb615beef6c49cc697c2853952a4ccfb43cec8
karan090909kmr-bit <karan090909kmr@gmail.com>
2026-02-17 20:01:13 +0530

    Revert the Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/">
+        <BrowserRouter >
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
